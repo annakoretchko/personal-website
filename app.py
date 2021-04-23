a318b5d1aeaee2ec5f1be48fc194c0feab40f730
Anna Koretchko <annakoretchko@gmail.com>
2021-04-23 14:15:33 -0400

    fixed contact me landing page direction

--- app.py
+++ app.py
@@ -2,9 +2,15 @@ from flask import Blueprint, Flask, g, jsonify, render_template, request, Respon
 from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
 import os
 import helper
+import pandas as pd
+from flask_mail import Mail, Message
 
 # Configure application
 app = Flask(__name__)
+
+
+SECRET_KEY = os.urandom(32)
+app.config['SECRET_KEY'] = SECRET_KEY
 
 @app.route('/index', methods=['POST', 'GET'])
 @app.route('/', methods=['POST', 'GET'])
@@ -31,22 +37,58 @@ def portfolio():
                             title="PROJECT PORTFOLIO",
                             id="portfolio")
 
-# @app.route('/about', methods=['POST', 'GET'])
-# def about():
+@app.route('/about', methods=['POST', 'GET'])
+def about():
 
-#     # get the title content for the portfolio page
-#     title_text = "about go here"
-#     # skills1= {"read": '1', "write": '4', "run": '5'}
-#     # skills2= {"code": '1', "bike": '4', "lift": '5'}
-#     # #skills = [skills1,skills2]
-#     # skills = {"code": 'test', "bike": '4', "lift": '5'}
-#     skills = helper.get_skill_content()
+    # get the title content for the portfolio page
+    title_text = "about go here"
+    # skills1= {"read": '1', "write": '4', "run": '5'}
+    # skills2= {"code": '1', "bike": '4', "lift": '5'}
+    # #skills = [skills1,skills2]
+    # skills = {"code": 'test', "bike": '4', "lift": '5'}
+    skills = helper.get_skill_content()
 
-#     return render_template('/about.html',
-#                             title_text=title_text,
-#                             skills = skills,
-#                             title="ABOUT ME",
-#                             id="about")
+    return render_template('/about.html',
+                            title_text=title_text,
+                            skills = skills,
+                            title="ABOUT ME",
+                            id="about")
+
+@app.route('/contact', methods=["GET","POST"])
+def contact():
+    title_text = "TITLE HERE"
+    form = helper.ContactForm()
+    if request.method == 'POST':
+        name =  request.form["name"]
+        email = request.form["email"]
+        subject = request.form["subject"]
+        message = request.form["message"]
+        res = pd.DataFrame({'name':name, 'email':email, 'subject':subject ,'message':message}, index=[0])
+        res.to_csv('./contactusMessage.csv')
+        return render_template('/contact.html', 
+                                form=form,
+                                title_text=title_text,
+                                title = "CONTACT TILEEEEE",
+                                id = "contact")
+    else:
+        return render_template('/contact.html', 
+                                form=form,
+                                title_text=title_text,
+                                title = "CONTACT TILEEEEE",
+                                id = "contact")
+
+
+
+
+
+
+@app.route("/robots.txt")
+def robots():
+    '''
+    Add robots.txt file to avoid google indexing
+    '''
+
+    return send_from_directory(app.static_folder, request.path[1:])
 
 
 if __name__ == "__main__":
